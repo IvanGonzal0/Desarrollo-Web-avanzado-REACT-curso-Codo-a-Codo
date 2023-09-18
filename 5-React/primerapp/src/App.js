71d1bb658f37d8fe0f51e86d6991aee104ac6320
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-09-18 14:03:55 -0300

    5-React+Bootstrap

---
+++ 5-React/primerapp/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Usuario from './components/Usuario'
+
+function App() {
+  return (
+    <div className="App">
+      <Usuario/>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola MUNDO REACT.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
