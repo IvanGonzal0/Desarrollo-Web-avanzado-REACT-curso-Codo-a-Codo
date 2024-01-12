5905e4818c1040db5115094bfbfd0ad8b6f805fd
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2024-01-11 22:20:22 -0300

    Editor de memes

---
+++ 15-Editor de memes/memes-master/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Cookies from './Componentes/Cookies';
+import Imgmeme from './Componentes/Imgmeme';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    <Cookies/>
+    <Imgmeme /> 
+
+    </div>
+  );
+}
+
+export default App;
