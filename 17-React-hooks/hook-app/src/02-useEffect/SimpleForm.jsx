71cef3b3e8e6e4a1108004323736fb1b96e55a7f
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2024-01-22 20:13:11 -0300

    useEffect dependencies

--- 17-React-hooks/hook-app/src/02-useEffect/SimpleForm.jsx
+++ 17-React-hooks/hook-app/src/02-useEffect/SimpleForm.jsx
@@ -1,26 +1,33 @@
 import { useEffect, useState } from "react";
 
 export default function SimpleForm() {
+  const [formState, setFormState] = useState({
+    username: "strider",
+    email: "ivangonzalo@gmail.com",
+  });
 
-    const [formState, setFormState] = useState({
-        username:'strider',
-        email:'ivangonzalo@gmail.com',
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
     });
+  };
 
-    const {username, email} = formState;
+  useEffect(() => {
+    console.log("useEffect called!");
+  }, []);
 
-    const onInputChange = ({target}) => {
-        const {name, value } = target
-        setFormState({
-            ...formState,
-            [name]: value
-        });
-    }
+  useEffect(() => {
+    console.log("formState changed!");
+  }, [formState]);
 
-    useEffect(() => {
-        console.log('useEffect called!');
-    })
-    
+  useEffect(() => {
+    console.log('email changed!');
+},[email]);
+
   return (
     <>
       <h1>Formulario simple</h1>
